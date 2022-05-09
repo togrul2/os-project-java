73ed0688749817deb067b20bd66b6c8d84625105
togrul2 <adsads00123@gmail.com>
2022-05-09 04:16:02 +0400

    init func

--- src/com/memory/Memory.java
+++ src/com/memory/Memory.java
@@ -3,6 +3,8 @@ package com.memory;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Memory {
     private final int V;
@@ -11,15 +13,21 @@ public class Memory {
     private final int P;
     private int offsetSize;
     private int[] virtualMemory;
-    private int[] pageTable;
-    private int[] memory;
+    private final Map<Integer, Integer> pageTable;
+    private int[] physicalMemory;
 
     private Memory(final int v, final int m, final int s, final int p) {
-        this.V = v;
-        this.M = m;
-        this.S = s;
-        this.P = p;
-        this.offsetSize = (int)(Math.log(this.P * 1024) / Math.log(2));
+        this.V = v * 1024;
+        this.M = m * 1024;
+        this.S = s * 1024;
+        this.P = p * 1024;
+        this.virtualMemory = new int[(int) (this.V / 4)];
+        this.physicalMemory = new int[(int) (this.M / 4)];
+        this.pageTable = new HashMap<>();
+        for (int i = 0; i < 64; i++) {
+            pageTable.put((i * this.P), ((i + 1) * this.P));
+        }
+        this.offsetSize = (int) (Math.log(this.P) / Math.log(2));
     }
 
     public static Memory init(final int v, final int m, final int s, final int p) {
@@ -37,6 +45,15 @@ public class Memory {
     public void read(final int a) {
         try {
             int x = 0;
+            int offset = (int) (a % Math.pow(2, this.offsetSize));
+            int vpn = (int) (a / Math.pow(2, this.offsetSize));
+            Integer ppn = pageTable.get(vpn);
+            if (ppn == null) {
+                // Page fault
+                this.log(String.format("Page fault at %d", a));
+            } else {
+                x = physicalMemory[ppn << this.offsetSize + offset];
+            }
             this.log(String.format("Value at address %d is %d", a, x));
         } catch (IOException e) {
             System.err.println("Error occurred while opening log file");
@@ -46,6 +63,11 @@ public class Memory {
 
     public void write(final int a, final int x) {
         try {
+            int offset = (int) (a % Math.pow(2, this.offsetSize));
+            int vpn = (int) (a / Math.pow(2, this.offsetSize));
+            //TODO some way to store vpn value
+            this.pageTable.put(vpn, 2);
+            Integer ppn = pageTable.get(vpn);
             this.log(String.format("Written %d to address %d", x, a));
         } catch (IOException e) {
             System.err.println("Error occurred while opening log file");
